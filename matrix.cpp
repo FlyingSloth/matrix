152e964ba9fa6a36bb7abf8a5ac86e13b7f012a7
Marina Verzh <m-verzh@rambler.ru>
2012-04-06 16:37:26 +0400

    +summ

--- matrix.cpp
+++ matrix.cpp
@@ -2,18 +2,36 @@
 #include "stdlib.h"
 #include "pthread.h"
 #include "matrix.h"
+#include <ctime>
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	Matrix m1;
 	m1 = readMatrix(argv[1],m1);
-	cout << m1.cols << " " << m1.rows << "\n";
-	for (int i = 0; i <m1.rows; i++)
+	Matrix m2;
+	m2 = readMatrix(argv[2],m2);
+	Matrix sum;
+	const clock_t start = clock();
+	sum = summ(m1,m2);
+	const double time = static_cast<double>(clock() - start);
+
+	//cout << sum.cols << " " << sum.rows << "\n";
+	for (int i = 0; i <sum.rows; i++)
 	{
-		for (int j = 0; j < m1.cols; j++)
+		for (int j = 0; j < sum.cols; j++)
+			cout << sum.matr[j][i] << " ";
+		cout << "\n";
+	}
+    cout << time << "\n";
+	/*
+	for (int i = 0; i <m1.cols; i++)
+	{
+		for (int j = 0; j < m1.rows; j++)
 			cout << m1.matr[i][j] << " ";
 		cout << "\n";
 	}
+	*/
+	return 0;
 }
 
--- matrix.h
+++ matrix.h
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
-#include "pthread.h"
+#include "omp.h"
 #include "fstream"
 #include "sstream"
 using namespace std;
@@ -21,7 +21,7 @@ Matrix readMatrix(char* adr, Matrix m)
 	m.rows = 0;
 	char* c;
 	char* buf;
-	double* tmp = new double[1000];
+	double* tmp = new double[1000000];
 	int col = 0;
 	int prevcol = 0;
 	int row = 0;
@@ -36,34 +36,58 @@ Matrix readMatrix(char* adr, Matrix m)
 		while(buf != NULL)
 		{
 			tmp[count] = atof(buf);
+			//cout << tmp[count] << " ";
 			count++;
 			col ++;
 			buf = strtok(NULL, " ");
 		}
 		row ++;
 		if ((prevcol != col) && (row != 1)) {cout << "Wrong array size!\n"; exit(1);};
+		//cout << "\n";
 	}
 
 	m.cols = col;
 	m.rows = row;
 	
+	//cout << col << " " << row << "\n";
 	m.matr = new double*[m.cols];
 	for (int i = 0; i < m.cols; i++)
 		m.matr[i] = new double[m.rows];
 		
-	for (int i = 0; i < m.rows; i++)
-		for (int j = 0; j < m.cols; j++)
+	for (int i = 0; i < m.cols; i++)
+		for (int j = 0; j < m.rows; j++)
 		{
-			m.matr[i][j] = tmp[j+i*m.cols];
+			m.matr[i][j] = tmp[i+j*m.cols];
+			//cout << "[" << i << "][" << j<< "]" << m.matr[i][j] << "\n";
 		}
 	delete[] tmp;
-
-	return m;
-	/*for (int i = 0; i < m.rows; i++)
+	/*
+	for (int i = 0; i < m.rows; i++)
 		for (int j = 0; j < m.cols; j++)
 		{
 			printf("[%d][%d] %f ", i, j, m.matr[i][j]);
 			cout << "\n";
-		}*/
-}
+		}
+	*/
+	return m;
+	
+};
+
+Matrix summ(Matrix m1, Matrix m2)
+{
+	if ((m1.cols != m2.cols) || (m1.rows != m2.rows))
+		{cout << "Array sizes don't match\n"; exit(1);};
+	
+	Matrix sum;
+	sum.matr = new double*[m1.cols];
+	for (int i = 0; i < m1.cols; i++)
+		sum.matr[i] = new double[m1.rows];
+		
+	#pragma omp parallel for
+	for (int i = 0; i < m1.cols; i++)
+		#pragma omp parallel for
+		for (int j = 0; j < m1.rows; j++)
+			sum.matr[i][j] = m1.matr[i][j] + m2.matr[i][j];
+	return sum;
+};
 
